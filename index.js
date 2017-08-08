2fdf1b7421d420dce878263cb62830529846c14d
William Macleod <willrmacleod@gmail.com>
2017-08-09 00:03:26 +0800

    implement callback

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 const app = express();
 
-//route handler
-app.get('/', (req ,res) => {
-    res.send({ hey: 'laura'});
-});
+passport.use(new GoogleStrategy({
+    //pass in config that tells this googlestrat how to auth users.
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+    }, (accessToken, refreshToken, profile, done) => {
+        console.log('access token :' + accessToken);
+        console.log('refresh token :' + refreshToken);
+        console.log('profile :' + profile);
+        console.log('done :' + done);
+        }
+    )
+);
+
+app.get('/auth/google', 
+    passport.authenticate('google', {
+        scope: ['profile', 'email']
+    })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+
 
 //dynamic port binding.
 //heroku will set an enviroment variable to bind the port to. 
