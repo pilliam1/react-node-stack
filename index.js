60cfefe9265239e205e308546060df9c99a29549
William Macleod <willrmacleod@gmail.com>
2017-08-09 00:35:47 +0800

    added console.log

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ passport.use(new GoogleStrategy({
         console.log('access token :' + accessToken);
         console.log('refresh token :' + refreshToken);
         console.log('profile :' + profile);
+        console.log('done :' + done);
         }
     )
 );
