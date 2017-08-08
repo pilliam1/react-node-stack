2f7e6bcbdd7e39cdb2bc13315ae5885325b695de
William Macleod <willrmacleod@gmail.com>
2017-08-09 00:53:32 +0800

    add comments

--- index.js
+++ index.js
@@ -18,12 +18,15 @@ passport.use(new GoogleStrategy({
     )
 );
 
+
 app.get('/auth/google', 
+                        //'google' is referring to GoogleStrategy
     passport.authenticate('google', {
         scope: ['profile', 'email']
     })
 );
 
+//create route handler for callback recieved from google once user grant permission
 app.get('/auth/google/callback', passport.authenticate('google'));
 
 
