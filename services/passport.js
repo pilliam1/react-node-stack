0ae43b03cee77edce9a33a57cd9ed99f92d892de
William Macleod <willrmacleod@gmail.com>
2017-08-12 15:15:55 +0800

    es2017 promises

--- services/passport.js
+++ services/passport.js
@@ -24,22 +24,20 @@ passport.use(new GoogleStrategy({
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback',
     proxy: true
-    }, (accessToken, refreshToken, profile, done) => {
-        User.findOne({ googleID: profile.id })
-        //promise
-            .then((existingUser) => {
-                if (existingUser){
-                    //already have record with profileID
-                    //1st arg means everything is fine
-                    //2nd arg means this is the user we found
-                    done(null, existingUser);
-                } else {
-                    //dont have user record, make a new one
-                    //creates mongoose model instance
-                    new User({ googleID: profile.id}).save()
-                        .then(user => done(null, user));
-                }
-            });
+    }, 
+    async (accessToken, refreshToken, profile, done) => {
+        const existingUser = await User.findOne({ googleID: profile.id });
+        
+        if (existingUser){
+            //already have record with profileID
+            //1st arg means everything is fine
+            //2nd arg means this is the user we found
+            done(null, existingUser);
+        } else {
+            //dont have user record, make a new one
+            //creates mongoose model instance
+            const user = await new User({ googleID: profile.id}).save();
+            done(null, user);
         }
-    )
-);
+    }
+ ));
