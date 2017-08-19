4a0760719929f39aa09907d2f12808770bc5a45a
William Macleod <willrmacleod@gmail.com>
2017-08-19 15:56:51 +0800

    added survey schema

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const passport = require('passport');
 const bodyParser = require('body-parser');
 const keys = require('./config/keys')
 require('./models/User');
+require('./mondels/Survey');
 require('./services/passport.js');
 
 mongoose.connect(keys.mongoURI);
